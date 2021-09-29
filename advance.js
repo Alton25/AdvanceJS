9ef231c92c378f52546c3e84801c46df3d8609f1
Brionna McElhaney <brionnapartee@gmail.com>
2021-09-29 09:30:01 -0500

    creating and understanding the fundamentals of JS

---
+++ advance.js
@@ -0,0 +1,43 @@
+//name = "Brionna";
+//var name;
+//console.log(name);
+setName();
+function setName(){
+var name = "Covalence";
+console.log(name);
+}
+console.log('some type of text');
+let avg = someAvg(1, 2);
+
+console.log('some type of text', avg);
+function someAvg(d, r){
+console.log('some type of text');
+var result = d + r / 2;
+return result;
+}
+
+let fruits = ['green apple', 'bananas', 'strawberry'];
+
+//function getFruit(){
+   // let favFRuit = fruits[1];
+    //favFRuit = fruits[2];
+    //console.log(fruits[0]);
+   // function getFavFruit(){
+       // console.log(favFRuit);
+      //  let leastFav;
+       // leastFav = fruits[2];
+  //  }
+   // console.log(leastFav);
+//}
+//getFruit();
+//getFavFruit();
+
+lastfunc();
+ function lastfunc(){
+console.log("Hello, Brionna");
+}
+babyBlues();
+function babyBlues(){
+    alert('my dog wants all the attention and the love');
+}
+
